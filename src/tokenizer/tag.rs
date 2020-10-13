6baea2f995ce4ca5776f37655542bd2514fd2aaa
Benjamin Minixhofer <bminixhofer@gmail.com>
2020-10-13 19:23:12 +0200

    add splitting heuristic for german

--- src/tokenizer/tag.rs
+++ src/tokenizer/tag.rs
@@ -1,4 +1,5 @@
 use super::WordData;
+use lazy_static::lazy_static;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufRead;
@@ -55,12 +56,52 @@ impl Tagger {
         Ok(Tagger { tags })
     }
 
-    pub fn get_tags(&self, word: &str) -> Vec<WordData> {
+    fn get_strict_tags(&self, word: &str) -> Vec<WordData> {
         let mut tags = self.tags.get(word).cloned().unwrap_or_else(Vec::new);
         let lower = word.to_lowercase();
 
         if word != lower && crate::utils::is_title_case(word) || crate::utils::is_uppercase(word) {
             tags.extend(self.tags.get(&lower).cloned().unwrap_or_else(Vec::new));
+        }
+
+        tags
+    }
+
+    pub fn get_tags(&self, word: &str) -> Vec<WordData> {
+        let mut tags = self.get_strict_tags(word);
+
+        lazy_static! {
+            static ref IS_GERMAN: bool = std::env::var("RULE_LANG").unwrap() == "de";
+        }
+
+        // compound splitting heuristic, seems to work reasonably well
+        if *IS_GERMAN && tags.is_empty() {
+            let n_chars = word.chars().count() as isize;
+
+            if n_chars >= 7 {
+                let indices = word
+                    .char_indices()
+                    .take(std::cmp::max(n_chars - 4, 0) as usize)
+                    .skip(1)
+                    .map(|x| x.0);
+
+                for i in indices {
+                    let next =
+                        crate::utils::apply_to_first(&word[i..], |c| c.to_uppercase().collect());
+                    let next_tags = self.get_strict_tags(&next);
+
+                    if !next_tags.is_empty() {
+                        tags = next_tags
+                            .into_iter()
+                            .map(|mut x| {
+                                x.lemma = format!("{}{}", &word[..i], x.lemma.to_lowercase());
+                                x
+                            })
+                            .collect();
+                        break;
+                    }
+                }
+            }
         }
 
         tags
