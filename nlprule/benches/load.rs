7ba1003758944226b9f6217281d521d251a41e1a
Bernhard Schuster <bernhard@ahoi.io>
2021-04-22 18:11:21 +0200

    chore: improve benches a bit (#71)

--- nlprule/benches/load.rs
+++ nlprule/benches/load.rs
@@ -1,15 +1,31 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use nlprule::{Rules, Tokenizer};
+use std::time::Duration;
 
-fn criterion_benchmark(c: &mut Criterion) {
+fn parse_tokenizer(c: &mut Criterion) {
     c.bench_function("load tokenizer", |b| {
         b.iter(|| Tokenizer::new(black_box("../storage/en_tokenizer.bin")).unwrap())
     });
+}
 
+fn parse_rules(c: &mut Criterion) {
     c.bench_function("load rules", |b| {
         b.iter(|| Rules::new(black_box("../storage/en_rules.bin")).unwrap())
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
-criterion_main!(benches);
+
+fn no_warmup_criterion() -> Criterion {
+	let crit = Criterion::default().sample_size(20).warm_up_time(Duration::from_nanos(1));
+	crit
+}
+
+criterion_group!(
+name = parse;
+config = no_warmup_criterion();
+targets =
+	parse_rules,
+	parse_tokenizer,
+);
+
+criterion_main!(parse);
